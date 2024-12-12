c5e1c5f9f79a31172e1d7f21840a38f09f1032d3
strahd <jv.palha@gmail.com>
2024-12-12 11:48:23 -0300

    add: babel to compatibility

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,12 @@ module.exports = {
     module: {
         rules: [
             {test: /\.css$/, use: ['style-loader', 'css-loader']},
+            {test: /\.js$/, exclude: /node_modules/, 
+                use: {
+                    loader: 'babel-loader', 
+                    options: {presets: ['@babel/preset-env']}
+                }
+            }
         ]
     }
 }
